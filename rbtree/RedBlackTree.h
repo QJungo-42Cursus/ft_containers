56acb6654ff7a69a23734d11c04f5613f938094b
Quentin <quentin.jungo@gmail.com>
2022-12-14 17:26:00 +0100

    nice

--- rbtree/RedBlackTree.h
+++ rbtree/RedBlackTree.h
@@ -11,10 +11,33 @@ public:
   vtype;
 
 public:
+  /// descend dans l'arbre jusqua l'endroit adequat pour place le new node
   void insert(value_type data) {
+    if (keyExist()) {
+      // TODO throw ??
+    }
     Node<T> *new_node = new Node<T>(data);
+
+	// TODO recursive insert
+	
+	if (!restoreValidity()) {
+		restoreValidity();
+	}
     return;
   }
+
+  /// renvoie true si la data/key exist deja
+  bool keyExist(value_type data) {}
+
+  /// effectue une rotation depuis this, dans la direction is_left
+  /// TODO dans node ! (ou pas ?)
+  void rotation(bool is_left) {}
+
+  /// renvoie true si l'arbre en valide
+  bool respectValidity() {}
+
+  /// check si l'arbre respect les regle du rbtree et le fais respecter si non
+  void restoreValidity() {}
 };
 
 #endif /* REDBLACKTREE_H */
